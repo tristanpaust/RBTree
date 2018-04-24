7254787330d9355fb5e43505c48f34e9a604caab
Tristan Paust <tristanpaust@gmail.com>
2018-04-23 21:59:09 -0700

    initial push for red-black trees

---
+++ rbtree.hs
@@ -0,0 +1,13 @@
+module RedBlackTrees where
+
+data Color = R | B 
+  deriving (Eq, Show)
+
+data RBTree a = L | N Color (RBTree a) a (RBTree a)
+  deriving (Show)
+
+find :: Ord a => a -> RBTree a -> Maybe a
+find x L = Nothing
+find x (N c t y u)
+  | x == y       = Just y
+  | otherwise = Nothing
